04b2c83e6d0af0a2037690b8463795bcf9015455
SandiJamlu23 <sandijamlu694@gmail.com>
2025-04-24 22:14:21 +0700

    railway setup

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_login import UserMixin, login_user, logout_user, login_required, current_user
-
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -13,11 +15,10 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__,
                 template_folder='templates',
-                static_folder='../static') # Point to the static folder at root level
-                                            # or move static folder under the app
+                static_folder='../static')
     
-    app.config['SECRET_KEY'] = 'dev-secret-key'  
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///passwords.db'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret-key')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///passwords.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
